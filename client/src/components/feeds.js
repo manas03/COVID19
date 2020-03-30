748ca4a4d0e64d3ea2b9af6c838cd3c996f97371
manas03 <03.msahai03@gmail.com>
2020-03-31 03:50:28 +0530

    Added twitter widget

--- client/src/components/feeds.js
+++ client/src/components/feeds.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-class Feeds extends Component {
-    render() {
-        return (
-            <div>
-            <p>Twitter</p>
-            <h1>H</h1>
-            <a class="twitter-timeline" data-width="400" data-height="400" data-theme="dark" href="https://twitter.com/WHO?ref_src=twsrc%5Etfw">Tweets by WHO</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </div>
-        )
-    }
-}
-export default Feeds
+const Feeds = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+    document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+  }, []);
+
+  return (
+    <section className="twitter-timeline py-5 col-md-6">
+      <div className="twitter-embed py-5  justify-content-center">
+        <Link
+          className="twitter-timeline"
+          data-theme="dark"
+          data-tweet-limit="10"
+          data-chrome="nofooter noborders"
+          to="https://twitter.com/WHO?ref_src=twsrc%5Etfw"
+        >
+          Tweets by WHO
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Feeds;
+
+//  <a class="twitter-timeline" data-width="400" data-height="400" data-theme="dark" href="https://twitter.com/WHO?ref_src=twsrc%5Etfw">Tweets by WHO</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
