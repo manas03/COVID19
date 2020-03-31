757fe13add5039d31b5610af2457572bb8d2efba
Rachit Singhal <rachitsinghal40@gmail.com>
2020-03-31 23:32:24 +0530

    Prepared for deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
-
+const path=require('path')
 const app = express();
 
 //body parser middleware
@@ -31,6 +31,14 @@ require("./config/passport")(passport);
 
 //Use routes
 app.use("/api/users", users);
+//Serve static asses
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'))
+ app.get('*',(req,res)=>{
+   res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+ })
+}
+
 
 const port = process.env.PORT || 5000;
 
