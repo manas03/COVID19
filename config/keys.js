85f5291b06bfd170487afd65a4096028b4a77f64
Rachit Singhal <rachitsinghal40@gmail.com>
2020-03-31 22:54:16 +0530

    Production started

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://manas:manas@cluster0-vlbyo.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+
+if(process.env.NODE_ENV==='production')
+{
+  module.exports=require('./keys_prod');
+} else {
+  module.exports=require('./keys_dev');
+}
